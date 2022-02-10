54faa7c4a64dddb9e081d6a11deff1c3055777f5
Joao Paulo Furtado <joaopaulofurtado@live.com>
2022-02-09 22:52:54 -0800

    created more tests

--- test/betting.test.js
+++ test/betting.test.js
@@ -1,3 +1,4 @@
+const _ = require('lodash');
 const Betting = artifacts.require("BettingSystem");
 const truffleAssert = require('truffle-assertions');
 
@@ -51,9 +52,57 @@ contract("Betting", async (accounts) => {
   });
 
   it("only managers can pick a bet winner", async () => {
-    
     // check if pickWinner is rejected 
-    await truffleAssert.fails(betting.pickWinner("Team A", { from: accounts[1] }));
-
+    await truffleAssert.fails(betting.pickWinner("Team B", { from: accounts[1] }));
   });
+
+  it("calculates payout correctly", async () => {
+
+    // redeploy contract so we can start bets over.
+    betting = await Betting.new(["Team A", "Team B"], { from: accounts[0] });
+
+    await betting.placeBet("Team A", { from: accounts[0], value: web3.utils.toWei("1", "ether") });
+    await betting.placeBet("Team B", { from: accounts[1], value: web3.utils.toWei("1", "ether") });
+    await betting.placeBet("Team A", { from: accounts[2], value: web3.utils.toWei("1", "ether") });
+    await betting.placeBet("Team B", { from: accounts[3], value: web3.utils.toWei("2", "ether") });
+ 
+    const initialAccount1Balance = await web3.eth.getBalance(accounts[1]);
+
+    await betting.pickWinner("Team B", { from: accounts[0] });
+
+    const finalAccount1Balance = await web3.eth.getBalance(accounts[1]);
+
+    const initialBalanceInETH = web3.utils.fromWei(initialAccount1Balance);
+    const finalBalanceInETH = web3.utils.fromWei(finalAccount1Balance);
+
+    const payout = _.floor(finalBalanceInETH - initialBalanceInETH, 2);
+
+    assert.ok(Number(finalAccount1Balance) > Number(initialAccount1Balance));
+    // check if the balance of account 1 has increased by 2 ether
+    
+    // balance should increase 1.66 ETH under these conditions
+    assert.equal(payout, 1.66);
+  });
+
+  it("should not allow a bet in a unexistent team", async () => {
+    await truffleAssert.fails(betting.placeBet("Team C", { from: accounts[0], value: web3.utils.toWei("1", "ether") }));
+  });
+
+  it("should require at least one team to be selected", async () => {
+    await truffleAssert.fails(betting.placeBet("", { from: accounts[0], value: web3.utils.toWei("1", "ether") }));
+  });
+
+  it("should refund all player if a draw is called", async () => {});
+
+  it("should not be able to place bets, once the contract is not active", async () => {
+
+    // redeploy contract so we can start bets over.
+    betting = await Betting.new(["Team A", "Team B"], { from: accounts[0] });
+
+    await betting.placeBet("Team A", { from: accounts[0], value: web3.utils.toWei("1", "ether") });
+
+    await betting.pickWinner("Team A", { from: accounts[0] });
+
+    await truffleAssert.fails(betting.placeBet("Team A", { from: accounts[0], value: web3.utils.toWei("1", "ether") }));
+  })
 });
